1e73875290748c6c6c74d324453379a920d06df2
Sampleeeee <behnersam@gmail.com>
2021-05-30 21:30:53 -0400

    Not sure why I did this lol

--- StaffCommands.cs
+++ StaffCommands.cs
@@ -14,25 +14,20 @@ namespace Gumbot
         [Command( "mute" )]
         [Description( "Mute Someone!" )]
         [RequirePermissions( Permissions.MuteMembers )]
-        private async Task MuteCommand( CommandContext ctx, DiscordMember member = null, string reason = "No reason given" )
+        private async Task MuteCommand( CommandContext ctx, DiscordMember member, string reason = "No reason given" )
         {
             await ctx.TriggerTypingAsync();
 
-            DiscordUser[] mentioned = ctx.Message.MentionedUsers.ToArray();
-            if ( member is null && mentioned.Length != 1 )
-            {
-                await ctx.RespondAsync( "You must mention a user to mute." );
-                return;
-            }
+            await member.GrantRoleAsync( ctx.Guild.GetRole( Program.Config.MutedRole ) );
+            await ctx.RespondAsync( $"{member.Mention} has been muted by {ctx.Message.Author.Mention} for reason: {reason}." );
+        }
 
-            DiscordMember target;
-
-            if ( member is null ) target = ( DiscordMember ) mentioned[0];
-            else target = member;
-
-            await target.GrantRoleAsync( ctx.Guild.GetRole( Program.Config.MutedRole ) );
-            await ctx.RespondAsync(
-                $"{target.Mention} has been muted by {ctx.Message.Author.Mention} for reason: {reason}." );
+        [Command( "unmute" )]
+        [Description( "Opossum is no longer drunk 0.o" )]
+        [RequirePermissions( Permissions.MuteMembers )]
+        private async Task UnmuteCommand( CommandContext ctx, DiscordMember member = null )
+        {
+            
         }
     }
 }
