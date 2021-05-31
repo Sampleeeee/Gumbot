bc6daf7118d1a6f9c4f91fcc52c62302350d8e39
Sampleeeee <behnersam@gmail.com>
2021-05-30 21:23:08 -0400

    Color Command

---
+++ FunCommands.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace Gumbot
+{
+    public class FunCommands : BaseCommandModule
+    {
+        [Command( "color" )]
+        [Description( "set your color" )]
+        public async Task ColorCommand( CommandContext ctx, string color )
+        {
+            await ctx.TriggerTypingAsync();
+
+            if ( Program.Config.Colors.ContainsKey( color ) )
+            {
+                foreach ( var role in ctx.Member.Roles )
+                    if ( Program.Config.Colors.ContainsValue( role.Id ) )
+                        await ctx.Member.RevokeRoleAsync( role );
+
+                await ctx.Member.GrantRoleAsync( ctx.Guild.GetRole( Program.Config.Colors[color] ) );
+            }
+            else
+            {
+                var colors = "";
+                foreach ( ( string name, ulong id ) in Program.Config.Colors )
+                    colors += name + ", ";
+
+                await ctx.RespondAsync( $"That color does not exist! Valid colors: {colors.TrimEnd( ',' )}" );
+            }
+        }
+    }
+}
